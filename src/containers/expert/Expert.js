5a98867f623ca96609666636b6c4e3fe06bd1ed9
Kentros Alexandros <Alexandros.KENTROS@intrasoft-intl.com>
2019-05-30 14:01:55 +0300

    Added React-Components

--- src/containers/expert/Expert.js
+++ src/containers/expert/Expert.js
@@ -1,18 +1,38 @@
 import React, { Component } from "react";
-import "./Expert.css"
+import "./Expert.css";
+import styled from "styled-components";
 
 const pStyle = {
   fontSize: "50px",
   textAlign: "center"
 };
 
+const Div = styled.div`
+  margin: 40px;
+  border: 5px outset pink;
+  &:hover {
+    background-color: yellow;
+  }
+`;
+
+const Paragraph = styled.p`
+  font-size: 15px;
+  text-align: center;
+`;
+
 class Expert extends Component {
   render() {
     return (
       <>
-        <div style={pStyle}> HALLO </div>
-        <div style={{textAlign:"center",color:'pink',fontSize:'45p'}}> HALLO</div>
-        <div className="pStyle"> HALLO 2 </div>
+        <div style={pStyle}> HALLO INLINE</div>
+        <div style={{ textAlign: "center", color: "pink", fontSize: "45px" }}>
+          {" "}
+          HALLO INLINE PINK{" "}
+        </div>
+        <div className="pStyle"> HALLO CSS STYLESEET </div>
+        <Div>
+          <Paragraph>Get started with styled-components 💅</Paragraph>
+        </Div>
       </>
     );
   }
